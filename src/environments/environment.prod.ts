f219300ed70d173add25acb392000e56695ec76a
janvi19 <42508832+janvi19@users.noreply.github.com>
2022-07-28 18:20:10 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'chat-demo-28fa4',
+    appId: '1:809506524324:web:d603c8f0e55b41e2fd6a31',
+    databaseURL: 'https://chat-demo-28fa4-default-rtdb.firebaseio.com',
+    storageBucket: 'chat-demo-28fa4.appspot.com',
+    apiKey: 'AIzaSyBaNEWunou-0doxr9OJSBMr4aGyWlijt-0',
+    authDomain: 'chat-demo-28fa4.firebaseapp.com',
+    messagingSenderId: '809506524324',
+    measurementId: 'G-80YKHXL4M9',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'chat-demo-28fa4',
+    appId: '1:809506524324:web:d603c8f0e55b41e2fd6a31',
+    databaseURL: 'https://chat-demo-28fa4-default-rtdb.firebaseio.com',
+    storageBucket: 'chat-demo-28fa4.appspot.com',
+    apiKey: 'AIzaSyBaNEWunou-0doxr9OJSBMr4aGyWlijt-0',
+    authDomain: 'chat-demo-28fa4.firebaseapp.com',
+    messagingSenderId: '809506524324',
+    measurementId: 'G-80YKHXL4M9',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
